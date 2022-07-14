dac44063d46ba2a2518fddbf75880d94b48ce897
David Faith <faithodesola@gmail.com>
2022-07-14 19:07:04 +0100

    Reorder Project cards

--- sections/Project.js
+++ sections/Project.js
@@ -8,6 +8,12 @@ const Project = () => {
       </h1>
       <div className="my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         <ProjectCard
+          photo="/scriptroom.png"
+          url="https://scriptroom.vercel.app"
+          title="Scriptroom"
+          description="Scriptroom is a platform where users can both share and find scripts of all types and genres"
+        />
+        <ProjectCard
           photo="/cloakio.png"
           url="https://cloak-io.vercel.app"
           title="Cloak.io"
@@ -15,11 +21,29 @@ const Project = () => {
 entrepreneurs."
         />
         <ProjectCard
+          photo="/jfo.png"
+          url="https://www.jfodesola.com"
+          title="JfOdesola"
+          description="The Official site for Pastor Johnson Funso Odesola"
+        />
+        <ProjectCard
+          photo="/rccg-map.png"
+          url="https://rccg-lp8.vercel.app"
+          title="RCCG LP8 Map"
+          description="This site displays all the RCCG parishes under Lagos Province 8 including their corresponding info and location"
+        />
+        <ProjectCard
           photo="/lifenaturals.png"
           url="https://life-naturals.web.app"
           title="Life Naturals"
           description="Life Naturals is a fully functional E-commerce website that was developed to both advertise
 and sell various health products to consumers"
+        />
+        <ProjectCard
+          photo="/gameflix.png"
+          url="https://github.com/Olatokumbo/Gameflix"
+          title="Gameflix"
+          description="Gameflix is a video game review mobile app where users can review various games of different genres"
         />
         <ProjectCard
           photo="/covid.png"
@@ -35,30 +59,6 @@ COVID-19 pandemic. It also includes various charts and maps to well visualize th
           title="Thumbshare"
           description="ThumbShare is a web app that lets its users share various photos with family and friends
 online"
-        />
-        <ProjectCard
-          photo="/jfo.png"
-          url="https://www.jfodesola.com"
-          title="JfOdesola"
-          description="The Official site for Pastor Johnson Funso Odesola"
-        />
-        <ProjectCard
-          photo="/rccg-map.png"
-          url="https://rccg-lp8.vercel.app"
-          title="RCCG LP8 Map"
-          description="This site displays all the RCCG parishes under Lagos Province 8 including their corresponding info and location"
-        />
-        <ProjectCard
-          photo="/gameflix.png"
-          url="https://github.com/Olatokumbo/Gameflix"
-          title="Gameflix"
-          description="Gameflix is a video game review mobile app where users can review various games of different genres"
-        />
-        <ProjectCard
-          photo="/scriptroom.png"
-          url="https://scriptroom.vercel.app"
-          title="Scriptroom"
-          description="Scriptroom is a platform where users can both share and find scripts of all types and genres"
         />
       </div>
     </div>
